faa154d43d3d6cabb5e0cb019890d01824946539
Niko Torvinen <torvnen@gmail.com>
2020-06-13 09:08:16 +0300

    Add build. Adjust gitignore accordingly.

---
+++ build/precache-manifest.514a5e0a7cb90712d02892bedfec7c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e45f800a388ed108cf9cfb75d9734a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "895d945405090569c610",
+    "url": "/static/css/main.6254dfff.chunk.css"
+  },
+  {
+    "revision": "56a06befdb80f936ecb7",
+    "url": "/static/js/2.c75de13e.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.c75de13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895d945405090569c610",
+    "url": "/static/js/main.43637656.chunk.js"
+  },
+  {
+    "revision": "f1b8cb8ba2ed8ff3eb19",
+    "url": "/static/js/runtime-main.58c4d41b.js"
+  }
+]);
