7a8e61b20a85ea4f496f029107c4a024450f84e3
Niko Torvinen <torvnen@gmail.com>
2020-06-16 19:39:39 +0300

    Vertical and horizontal lines

--- src/components/LineComponent.tsx
+++ src/components/LineComponent.tsx
@@ -9,8 +9,7 @@ const LINE_OPACITY = 0.75;
 export const LineComponent = observer((props: LineComponentProps) => {
   const { game, model } = props;
   const { lines } = game;
-  const [style, setStyle] = React.useState<CSSProperties>({
-  });
+  const [style, setStyle] = React.useState<CSSProperties>({});
   React.useEffect(() => {
     //const cells = model.coords.map((v, i, a) => game.cellAt(v.x, v.y));
     if (!!model) {
@@ -26,57 +25,74 @@ export const LineComponent = observer((props: LineComponentProps) => {
       ) {
         const firstTd = firstTdRef.current;
         const lastTd = lastTdRef.current;
-        let [offsetX, y1, y2, w, h, deg] = [0, 0, 0, 0, 0, 0, 0];
+        let transformOrigin = dd.isDiagonal
+          ? `${dd.isRight ? "0%" : "100%"} ${dd.isDown ? " 0%" : " 100%"}`
+          : dd.isVertical
+          ? `0% ${dd.isDown ? "0%" : "100%"}`
+          : dd.isHorizontal
+          ? `0% 50%`
+          : undefined;
+        let [offsetX, offsetY, length, deg] = [0, 0, 0, 0];
         if (dd.isHorizontal) {
           console.log(
             "firstTd.offsetLeft, lastTd.offsetLeft",
             firstTd.offsetLeft,
             lastTd.offsetLeft
           );
-          h = LINE_THICKNESS;
-          y1 = y2 = firstTd.offsetTop + firstTd.offsetHeight / 2;
+          offsetY = firstTd.offsetTop + firstTd.offsetHeight / 2;
 
           if (dd.isRight) {
-            w = Math.abs(firstTd.offsetLeft - lastTd.offsetLeft) +
+            length =
+              Math.abs(firstTd.offsetLeft - lastTd.offsetLeft) +
               0.5 * firstTd.offsetWidth;
             deg = 0;
             offsetX = firstTd.offsetLeft + 0.25 * firstTd.offsetWidth;
           } else {
-            w = Math.abs(lastTd.offsetLeft - firstTd.offsetLeft) +
+            length =
+              Math.abs(lastTd.offsetLeft - firstTd.offsetLeft) +
               0.5 * firstTd.offsetWidth;
             deg = 180;
             offsetX = firstTd.offsetLeft + 0.75 * firstTd.offsetWidth;
           }
         } else if (dd.isVertical) {
-          offsetX = firstTd.offsetLeft + firstTd.offsetWidth / 2;
+          offsetX = (firstTd.offsetLeft + firstTd.clientWidth / 2) + 1;
+          length = Math.abs((firstTd.offsetTop - lastTd.offsetTop)) + 0.5 * firstTd.clientHeight
           if (dd.isUp) {
-            y1 = firstTd.offsetTop + firstTd.offsetHeight;
+            offsetY = firstTd.offsetTop + 0.75 * firstTd.offsetHeight;
+            deg = -90
           } else {
+            offsetY = firstTd.offsetTop + 0.25 * firstTd.offsetHeight;
+            deg = 90
           }
+        } else {
         }
         setStyle({
           ...style,
           left: offsetX,
-          width: w,
-          height: h,
-          top: y1,
-          transformOrigin: "0% 50%",
+          width: length,
+          height: LINE_THICKNESS,
+          top: offsetY,
+          transformOrigin,
           transform: `rotate(${deg}deg)`,
           position: "absolute",
         });
       }
     }
   }, [model, game, model.coords]);
-  return <div style={style}>
-    <div style={{
-      animationName: 'grow',
-      opacity: LINE_OPACITY,
-      animationDuration: '1s',
-      animationTimingFunction: "cubiz-bezier(0, 20, 50, 90)",
-      background: "green",
-      height: LINE_THICKNESS
-    }}></div>
-  </div>;
+  return (
+    <div style={style}>
+      <div
+        style={{
+          animationName: "grow",
+          opacity: LINE_OPACITY,
+          animationDuration: "1s",
+          animationTimingFunction: "cubiz-bezier(0, 20, 50, 90)",
+          background: "green",
+          height: LINE_THICKNESS,
+        }}
+      ></div>
+    </div>
+  );
 });
 
 export interface LineComponentProps {
