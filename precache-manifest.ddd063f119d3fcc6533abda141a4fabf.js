1aaf9d225429538b681ce02a30f88a841e224767
Niko Torvinen <torvnen@gmail.com>
2020-06-18 17:31:57 +0300

    Updates

---
+++ precache-manifest.ddd063f119d3fcc6533abda141a4fabf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8919b26302e37f32d26aa04d9c0aa78",
+    "url": "/LineGame/index.html"
+  },
+  {
+    "revision": "a5a94cac1b6d75d29853",
+    "url": "/LineGame/static/css/main.10691659.chunk.css"
+  },
+  {
+    "revision": "c284d65bf153be0989b7",
+    "url": "/LineGame/static/js/2.013540f7.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/LineGame/static/js/2.013540f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a94cac1b6d75d29853",
+    "url": "/LineGame/static/js/main.c25f4c1c.chunk.js"
+  },
+  {
+    "revision": "92a6f5d38b370ca5ad60",
+    "url": "/LineGame/static/js/runtime-main.6b2b2039.js"
+  },
+  {
+    "revision": "2aa8a682b44cecd91e449fecad4d8784",
+    "url": "/LineGame/static/media/DMMono-Light.2aa8a682.ttf"
+  },
+  {
+    "revision": "5701846e8683c78b137d971d095fb75f",
+    "url": "/LineGame/static/media/DMMono-MediumItalic.5701846e.ttf"
+  }
+]);
